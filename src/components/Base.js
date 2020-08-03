b782ab89d7a665e40477df3ecd91ca3115dff64d
SameerBatla <sambatla456@gmail.com>
2020-08-03 19:38:12 +0530

    framer with transition properties

--- src/components/Base.js
+++ src/components/Base.js
@@ -5,7 +5,12 @@ const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
-    <div className="base container">
+    <motion.div
+      className="base container"
+      initial={{ x: '250vw' }}
+      animate={{ x: 0 }}
+      transition= {{type:'spring', stiffness:120 ,delay:0.2}}
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map((base) => {
@@ -19,13 +24,18 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div className="next" initial={{ x: '-250vw' }} animate={{ x: 0 }}>
+        <motion.div
+          className="next"
+          initial={{ x: '-250vw' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 120 }}
+        >
           <Link to="/toppings">
             <button>Next</button>
           </Link>
         </motion.div>
       )}
-    </div>
+    </motion.div>
   );
 };
 
